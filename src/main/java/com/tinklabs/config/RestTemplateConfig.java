3748e00aca09f68136db8ffc0694b3db1e2607bd
landin_lin <landin.lin@tinklabs.com>
2019-01-08 17:30:04 +0800

    first commit

---
+++ src/main/java/com/tinklabs/config/RestTemplateConfig.java
@@ -0,0 +1,10 @@
+package com.tinklabs.config;
+
+/**
+ * @program: ruleserver
+ * @description:
+ * @author: Landin
+ * @create: 2019-01-04 20:37
+ **/
+public class RestTemplateConfig {
+}
---
+++ src/main/java/com/tinklabs/dto/AddressType.java
@@ -0,0 +1,10 @@
+package com.tinklabs.dto;
+
+/**
+ * @program: ruleserver
+ * @description:
+ * @author: Landin
+ * @create: 2019-01-04 21:00
+ **/
+public class AddressType {
+}
